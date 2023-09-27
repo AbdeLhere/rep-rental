6b946bb010b467a7727ea5d4a34e74e70ca1dbd0
abdel4999 <140607607+AbdeLhere@users.noreply.github.com>
2023-09-27 18:05:30 +0200

    Add files via upload
    giveRentalPaper with info

---
+++ server.lua
@@ -0,0 +1,85 @@
+local Core = exports['qb-core']:GetCoreObject()
+local _lang = GetConvar('repscripts:locale', 'en')
+local Lang = Config.Lang[_lang]
+local vehs = {}
+Core.Functions.CreateCallback('rep-rental:callback:checkLicense', function(source, cb, type)
+    local src = source
+    local Player = Core.Functions.GetPlayer(src)
+    local licenseTable = Player.PlayerData.metadata["licences"]
+    cb(licenseTable[type])
+end)
+
+RegisterServerEvent('rep-rental:server:giveRentalPaper', function(plate, model)
+    local src = source
+    local Player = Core.Functions.GetPlayer(src)
+    local info = {}
+   -- info.citizenid = Player.PlayerData.citizenid
+    info.firstname = Player.PlayerData.charinfo.firstname
+    info.lastname = Player.PlayerData.charinfo.lastname
+    info.plate = plate
+    info.time = TimeAmount
+   -- info.price = RentPrice
+    info.vehiclename = model
+
+    Player.Functions.AddItem('renting_contract', 1, nil, info)
+    TriggerClientEvent('inventory:client:ItemBox', source, Core.Shared.Items["renting_contract"], "add")
+
+end)
+
+Core.Functions.CreateCallback('rep-rental:callback:spawnVeh', function(source, cb, _data)
+    local src = source
+    local Player = Core.Functions.GetPlayer(src)
+    local ped = GetPlayerPed(src)
+    if _data.payment == 'cash' then
+        if Player.PlayerData.money.cash < _data.total then
+            TriggerClientEvent('Core:Notify', src, Lang['error_cash'].msg, Lang['error_cash'].type,
+                Lang['error_cash'].time)
+            cb(false)
+            return
+        end
+        Player.Functions.RemoveMoney('cash', _data.total)
+    else
+        if Player.PlayerData.money.bank < _data.total then
+            TriggerClientEvent('Core:Notify', src, Lang['error_bank'].msg, Lang['error_bank'].type,
+                Lang['error_bank'].time)
+            cb(false)
+            return
+        end
+        Player.Functions.RemoveMoney('bank', _data.total)
+    end
+    _data.model = type(_data.model) == 'string' and joaat(_data.model) or _data.model
+    local veh = CreateVehicle(_data.model, _data.coords.x, _data.coords.y, _data.coords.z, _data.coords.w, true, true)
+    while not DoesEntityExist(veh) do Wait(0) end
+    while GetVehiclePedIsIn(ped) ~= veh do
+        Wait(0)
+        TaskWarpPedIntoVehicle(ped, veh, -1)
+    end
+    while NetworkGetEntityOwner(veh) ~= src do Wait(0) end
+    vehs[#vehs + 1] = {
+        entity = veh,
+        time = os.time() + (_data.time * 60 * 60)
+    }
+    cb(NetworkGetNetworkIdFromEntity(veh))
+end)
+
+CreateThread(function()
+    while true do
+        for k, v in pairs(vehs) do
+            if v then
+                if os.time() >= v.time then
+                    if DoesEntityExist(v.entity) then
+                        DeleteEntity(v.entity)
+                    end
+                    vehs[k] = nil
+                end
+            end
+        end
+        Wait(60 * 1000)
+    end
+end)
+
+--Core.Functions.CreateUseableItem("renting_contract", function(source, item)
+    --if item.info and item.info.plate then
+    --    TriggerEvent("vehiclekeys:client:SetOwner", item.info.plate)
+  --  end
+--end)
